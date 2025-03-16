9152f1c18b638fe3ad1b55299719ed38aa9b52b8
Kiirogami <tranmh2002@gmail.com>
2025-03-16 22:35:48 +0100

    copy lemone

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# qmk_sofle
+
+Add submodule to the `qmk_firmware` repo:
+```
+git submodule add https://github.com/kiirogami/qmk_sofle.git keyboards/sofle/keymaps/kiiro
+```
+
+Compile/flash the layout:
+```
+qmk compile -kb sofle/rev1 -km kiiro
+qmk flash -kb sofle/rev1 -km kiiro
+```
---
+++ oled.c
@@ -0,0 +1,47 @@
+#include "luna.c"
+
+static void print_status_narrow(void) {
+    oled_write_P(PSTR("\n\n"), false);
+    oled_write_ln_P(PSTR("Kiiro\n"), false);
+
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_ln_P(PSTR("Default"), false);
+            break;
+        case _CHARS:
+            oled_write_ln_P(PSTR("chars"), false);
+            break;
+        case _VIM:
+            oled_write_ln_P(PSTR("nvim"), false);
+            break;
+        case _NUMPAD:
+            oled_write_ln_P(PSTR("num"), false);
+            break;
+        default:
+            oled_write_ln_P(PSTR("Undef"), false);
+    }
+
+    oled_set_cursor(0, 5);
+    oled_write("cpslk", led_usb_state.caps_lock);
+
+    render_luna(0, 13);
+}
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (is_keyboard_master()) {
+        return OLED_ROTATION_270;
+    }
+    return rotation;
+}
+
+bool oled_task_user(void) {
+    current_wpm   = get_current_wpm();
+    led_usb_state = host_keyboard_led_state();
+
+    if (is_keyboard_master()) {
+        print_status_narrow();
+    } else {
+        // render_logo();
+    }
+    return false;
+}
