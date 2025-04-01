c8ae58cf87f19cbfa20f47e753139f4f570a474e
Kiirogami <tranmh2002@gmail.com>
2025-04-01 10:19:06 +0200

    bugfixes

--- keymap.c
+++ keymap.c
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX,   KC_P7,   KC_P8,   KC_P9, KC_PPLS, _______,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                         XXXXXXX,   KC_P4,   KC_P5,   KC_P6, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,       XXXXXXX, XXXXXXX,   KC_P1,   KC_P2,   KC_P3, XXXXXXX, _______,
-                          _______, _______, _______, _______, _______,       _______, _______,   KC_P0  KC_PDOT, XXXXXXX
+                          _______, _______, _______, _______, _______,       _______, _______,   KC_P0,  KC_PDOT, XXXXXXX
       ), 
     
     /* _TETRIS
